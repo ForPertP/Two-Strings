3c5bae8d82b05eb2a1088bd936eb44694ac7d5fc
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-07-13 04:24:00 +0900

    Update Two-Strings.cpp

--- Two-Strings.cpp
+++ Two-Strings.cpp
@@ -1,1 +1,21 @@
+string twoStrings(string s1, string s2)
+{
+    string result{ "NO" };
+    unordered_map<char, int> um_words;
 
+    for (auto& it : s1)
+    {
+        um_words[it]++;
+    }
+
+    for (auto& it : s2)
+    {
+        if (um_words[it] > 0)
+        {
+            result = "YES";
+            break;
+        }
+    }
+
+    return result;
+}
