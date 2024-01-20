7e38ff761f18d6b87b2f8d2711ce2d3ab523e91d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-01-21 06:33:40 +0900

    Update Two-Strings.cpp

--- Two-Strings.cpp
+++ Two-Strings.cpp
@@ -14,24 +14,71 @@ string rtrim(const string &);
  *  2. STRING s2
  */
 
-string twoStrings(string s1, string s2)
+string twoStrings2(string s1, string s2)
 {
-    string result{ "NO" };
-    std::unordered_map<char, int> um_words;
-
-    for (auto& it : s1)
+    std::unordered_map<char, int> charCountMap;
+    
+    for (const auto& ch : s1)
     {
-        um_words[it]++;
+        charCountMap[ch]++;
     }
 
-    for (auto& it : s2)
+    for (const auto& ch : s2)
     {
-        if (um_words[it] > 0)
+        if (charCountMap[ch] > 0)
         {
-            result = "YES";
-            break;
+            return "YES";
         }
     }
+    
+    return "NO";
+}
 
-    return result;
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s1;
+        getline(cin, s1);
+
+        string s2;
+        getline(cin, s2);
+
+        string result = twoStrings(s1, s2);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
